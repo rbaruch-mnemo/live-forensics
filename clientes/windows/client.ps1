ffab027b3144a4b41c193bc8f3e734f3630ef79a
avalverde-mnemo <a.valverdemartinez@mnemo.com>
2022-08-16 10:54:54 -0500

    update

--- clientes/windows/client.ps1
+++ clientes/windows/client.ps1
@@ -1,7 +1,7 @@
 # Configuración inicial. Creacion de usuario grrserver con permisos
-$user = "grrclient"
+$user = "grragent"
 $password = ConvertTo-SecureString "hola123.," -AsPlainText -Force
-New-LocalUser -Name $user -Password $password -FullName "grr" -Description "grr client user"
+New-LocalUser -Name $user -Password $password -FullName $user -Description $user
 Add-LocalGroupMember -Group "Administrators" -Member $user
 Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n192.168.100.100`tgrrserver" -Force
 # SSH config para grrserver
@@ -11,10 +11,16 @@ Set-Service -Name sshd -StartupType 'Automatic'
 # Generación de llave y envío a servidor
 $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $user,$password
 #$secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
-$Password1 = "hola123.,"
-$User1 = "forense"
-$Credentials = New-Object System.Management.Automation.PSCredential($user1, $password1)
-$session = New-SSHSession -ComputerName grrserver -Credential $Credentials
-#Invoke-SSHCommand -Index $session.sessionid -Command "type id_rsa.pub | ssh -o 'StrictHostKeyChecking No' forense@grrserver 'cat >> .ssh/authorized_keys'" # Invoke Command Over SSH
-$cmds = {"New-Item $home\.ssh -Type Directory";"cd ~\.ssh; ssh-keygen -b 4096 -t rsa -f id_rsa -q -N 'hola123.,'";"type id_rsa.pub | ssh -o 'StrictHostKeyChecking No' forense@grrserver 'cat >> .ssh/authorized_keys'"}
+#Invoke-SSHCommand -Index $session.sessionid -Command "cat /home/$user/.ssh/id_rsa.pub"#ssh forense@grrserver cat C:\Users\$user\.ssh\id_rsa.pub >> /home/forense/.ssh/id_rsa.pub authorized_keys" # Invoke Command Over SSH
+$cmds = {"New-Item $home\.ssh -Type Directory";"cd ~\.ssh; ssh-keygen -b 4096 -t rsa -f id_rsa -q -N 'hola123.,'"}
+#'-type id_rsa.pub | ssh -o 'StrictHostKeyChecking No' forense@grrserver 'cat >> .ssh/authorized_keys'
 Start-Process powershell -Credential $cred -ArgumentList $cmds -Wait -NoNewWindow
+
+
+$Password1 = ConvertTo-SecureString "hola123.," -AsPlainText -Force
+$User1 = "forense"
+$Credentials = New-Object System.Management.Automation.PSCredential($User1, $Password1)
+$session = New-SSHSession -ComputerName grrserver -Credential $Credentials
+$cmds2 = {"cd ~\.ssh"; "type id_rsa.pub | ssh -o 'StrictHostKeyChecking No' forense@grrserver 'cat >> .ssh/authorized_keys'"}
+Start-Process powershell -Credential $cred -ArgumentList $cmds2 -Wait -NoNewWindow
+#Invoke-SSHCommand -Index $session.sessionid -Command "type id_rsa.pub | ssh -o 'StrictHostKeyChecking No' forense@grrserver 'cat >> .ssh/authorized_keys'" # Invoke Command Over SSH
